26e8e4ed3dff2a8a4b69c7b2469ef318498119aa
kartnyk <71129045+kartnyk@users.noreply.github.com>
2024-06-06 16:23:29 +0530

    Create HomeController.java

---
+++ HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model, @AuthenticationPrincipal OidcUser oidcUser) {
+        if (oidcUser != null) {
+            model.addAttribute("name", oidcUser.getFullName());
+        }
+        return "index";
+    }
+}
