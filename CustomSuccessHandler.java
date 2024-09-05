68ed11be77be1b23ef9eaf16a5a087d84c1f6533
kartnyk <71129045+kartnyk@users.noreply.github.com>
2024-09-05 18:15:48 +0530

    Update CustomSuccessHandler.java

--- CustomSuccessHandler.java
+++ CustomSuccessHandler.java
@@ -4,7 +4,8 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.oidc.OidcIdToken;
-import org.springframework.security.oauth2.core.oidc.OidcUser;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -21,23 +22,24 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-        if (authentication instanceof OAuth2AuthenticationToken) {  // Correct type check
+        if (authentication instanceof OAuth2AuthenticationToken) {
             OAuth2AuthenticationToken oauthToken = (OAuth2AuthenticationToken) authentication;
 
-            // Log the entire authentication object
-            logger.info("Authentication Object: " + oauthToken.toString());
+            // Extract principal
+            OAuth2User oauth2User = oauthToken.getPrincipal();
 
-            // Extract token values if they exist
-            if (oauthToken.getPrincipal() instanceof OidcUser) {
-                OidcUser oidcUser = (OidcUser) oauthToken.getPrincipal();
-                OAuth2AccessToken accessToken = oauthToken.getAccessToken();  // Get access token
+            if (oauth2User instanceof OidcUser) {
+                OidcUser oidcUser = (OidcUser) oauth2User;
                 OidcIdToken idToken = oidcUser.getIdToken();
+                OAuth2AccessToken accessToken = oauthToken.getAccessToken();  // Get access token from the token itself
 
+                // Log token values
                 logger.info("Access Token Value: " + (accessToken != null ? accessToken.getTokenValue() : "No access token"));
                 logger.info("ID Token Value: " + (idToken != null ? idToken.getTokenValue() : "No ID token"));
-                // No refresh token in OIDC context usually
             } else {
-                logger.info("User is not authenticated with an OpenID Connect provider.");
+                // If it's a plain OAuth2User, we can still log user attributes
+                logger.info("User is authenticated as an OAuth2User.");
+                logger.info("User Attributes: " + oauth2User.getAttributes().toString());
             }
         } else {
             logger.info("Authentication object is not an instance of OAuth2AuthenticationToken.");
