60ba163cd7295b322a31062e51dbec32ab58c580
kartnyk <71129045+kartnyk@users.noreply.github.com>
2024-10-18 15:19:45 +0530

    Create Oidcmodel.java

---
+++ Oidcmodel.java
@@ -0,0 +1,16 @@
+public class OidcDetailsRequest {
+
+    private String authId;
+    private String appCode;
+    private String emailId;
+    private String provider;
+    private String providerId;
+    private String accessToken;
+    private String refreshToken;
+    private Timestamp tokenExpiry;
+    private String idToken;
+    private String scope;
+    private String enabledFlg;
+
+    // Getters and Setters
+}
